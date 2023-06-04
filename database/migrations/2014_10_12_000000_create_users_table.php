2f5481ae85ac0564a88c5975520c7145fc2f576d
Manuel Albani <mnlalbani@gmail.com>
2023-06-04 19:55:13 -0400

    user migration fixes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,18 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('apellido');
+            $table->string('ci');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->binary('imagen');
+            $table->integer('rol_id');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+
+        $table->foreign('rol_id')->references('id')->on('rol');
     }
 
     /**
