c37723bfb7144da62705763f4123fe59d202e5f8
Manuel Albani <mnlalbani@gmail.com>
2023-06-25 12:00:22 -0400

    added file type table seeder

---
+++ database/seeders/TypeTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Type;
+
+class TypeTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {   Type::create([
+            'name' => 'manual_f',
+    ]);
+       Type::create([
+            'name' => 'manual_p',
+    ]);
+       Type::create([
+            'name' => 'guia',
+    ]);
+       Type::create([
+            'name' => 'presentacion',
+    ]);
+        
+    }
+}
