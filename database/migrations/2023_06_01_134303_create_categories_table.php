9fdff06a9f258544d40d05492c124621e10786ae
Manuel Albani <mnlalbani@gmail.com>
2023-06-28 12:49:55 -0400

    added softdelete column to categories table

--- database/migrations/2023_06_01_134303_create_categories_table.php
+++ database/migrations/2023_06_01_134303_create_categories_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id('id');
             $table->timestamps();
+            $table->softDeletes();
             $table->string('name');
         });
     }
