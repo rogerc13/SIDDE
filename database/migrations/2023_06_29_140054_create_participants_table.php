3f8c1c974a0969d3ea1973f5f4ee6912549bf3b3
Manuel Albani <mnlalbani@gmail.com>
2023-06-29 10:11:59 -0400

    added participants table migration

---
+++ database/migrations/2023_06_29_140054_create_participants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('person_id')->references('id')->on('people');
+            $table->foreignId('participant_status_id')->references('id')->on('participant_status');
+            $table->foreignId('scheduled_course_id')->references('id')->on('scheduled_course');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+};
