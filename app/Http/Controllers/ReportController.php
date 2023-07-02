ad6105329d8697fe7a5fbf0fda59e0ed997a4f80
Manuel Albani <mnlalbani@gmail.com>
2023-07-02 08:42:23 -0400

    reports draft

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,27 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Curso;
-use App\Models\CursoProgramado;
+use App\Models\Scheduled;
+use App\Models\Course;
+use App\Models\Participant;
 
 class ReportController extends Controller
 {
     public function test()
     {
-        //Courses by dates;
-        /* 
-        $start_date = date($request->start_time);
-        $end_date = date($request->end_time);
-        */
-
-        $inittime= date('2017-06-30');
-        $finitttime= date('2023-06-17');
-        $cursosProgramados = CursoProgramado::whereColumn([['fecha_f', '>=', 'fecha_i'], ['fecha_i', '<=', 'fecha_i']])->get();
-
+        //By date
+        $scheduledCourse = Scheduled::where('start_date',$request->start_date)
+        ->where('end_date',$request->end_date)->get();
+        //Quanity By Category
+        $categoryCourse = Course::where('category_id',$request->category_id)->get();
+        //Quantity By Status
+        $statusCourse = Scheduled::with('course_status')
+        ->where('course_status_id',$request->course_status_id)
+        ->get();
+        //Canceled Courses
+        $canceledCourse = Scheduled::where('course_status_id',4)->get();
+        //Participants by Status
+        $statusParticipant = Participant::where('participant_status_id')->get();
+        //Course Total Time
+        $timeCourse = Course::where('duration')->get();
         
-        dd($cursosProgramados->curso);
-        //dd($cursosProgramados);
-        //return view('pages.admin.usuarios.reports')->with('cursosProgramados',$cursosProgramados);
-        //return json_encode($cursosProgramados);
+        dd($scheduledCourse);
+        return view('pages.admin.usuarios.reports')
+        ->with('scheduledCourses',$scheduledCourse);
+
+        return ;
     }
 }
