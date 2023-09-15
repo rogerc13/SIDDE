986546c6d74eb6f733a309b7740bb12a400111a0
Manuel Albani <mnlalbani@gmail.com>
2023-09-14 21:31:07 -0400

    pdf tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Spatie\Browsershot\Browsershot;
 
 class ExampleTest extends TestCase
 {
@@ -12,10 +13,20 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_the_application_returns_a_successful_response()
+    /* public function test_the_application_returns_a_successful_response()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    } */
+
+    public function test_example(){
+        
+        //$html = view('acciones_formacion')->render();
+
+        
+
+        Browsershot::html('<h1>HELLO</h1>')
+        ->save(public_path('storage/example_with_auth.png'));
     }
 }
