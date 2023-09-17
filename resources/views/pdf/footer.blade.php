f8ce95872b3ba8ba630820b3a9e81aa89d377f98
Manuel Albani <mnlalbani@gmail.com>
2023-09-16 22:39:03 -0400

    pdf footer blade file

---
+++ resources/views/pdf/footer.blade.php
@@ -0,0 +1,27 @@
+<style>
+    * {
+        box-sizing: content-box;
+    }
+    footer {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+    }
+
+    footer>div {
+        width: 100%;
+        height: 20px;
+        font-size: 10rem;
+        margin-right: 30px;
+        display: flex;
+        justify-content: flex-end;
+
+    }
+</style>
+<footer>
+    <div>
+        <span class="pageNumber"></span>
+        <span> / </span>
+        <span class="totalPages"></span>
+    </div>
+</footer>
