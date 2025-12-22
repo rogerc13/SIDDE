820358a6bdfd55306c56f5ab086679149c5daf5d
Manuel Albani <mnlalbani@gmail.com>
2025-12-22 18:55:33 -0400

    evaluate participant button in participant list for facilitator

--- resources/views/pages/admin/participantescursos/index.blade.php
+++ resources/views/pages/admin/participantescursos/index.blade.php
@@ -11,7 +11,11 @@
     <a href="javascript:asignarParticipanteLista('{{url('u/af_programadas/'.$cursoprogramado->id.'/asignarParticipante')}}','{{$cursoprogramado->id}}')" value="{{$cursoprogramado->id}}" class="btn btn-blue {{$cursoprogramado->atMaxCapacity() ? 'disabled' :''}}"><i class="fa fa-plus" aria-hidden="true"></i> Asignar participante</a>
     <a href="javascript:crearParticipante('{{url('u/af_programadas/'.$cursoprogramado->id.'/participantes')}}')" class="btn btn-success {{$cursoprogramado->atMaxCapacity() ? 'disabled' :''}}"><i class="fa fa-plus" aria-hidden="true"></i> Registrar nuevo participante</a>        
   @else
-    <div class="btn btn-success evaluate-button {{!($cursoprogramado->isEnCurso() || $cursoprogramado->isCulminado()) ? 'disabled' : '' }}"><i class="fa fa-check" aria-hidden="true"></i><span> Evaluar Participantes</span></div>
+    @if ($cursoprogramado->isEnCurso() || $cursoprogramado->isCulminado())
+      <div class="btn btn-success evaluate-button"><i class="fa fa-check" aria-hidden="true"></i><span> Evaluar Participantes</span></div>
+    @else
+      <div class="btn btn-success disabled" aria-disabled="true"><i class="fa fa-check" aria-hidden="true"></i><span> Evaluar Participantes</span></div>
+    @endif
   @endif
   <a href="{{url('u/pdf/participants/'.$cursoprogramado->id)}}" class="btn btn-success"><i class="fa fa-file-pdf-o " aria-hidden="true"></i> Imprimir Lista de Participantes</a>
   <br>
