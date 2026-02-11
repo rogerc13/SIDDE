92b7f46b3612a0f64f9794a41ae8bd004c27ff27
Manuel Albani <mnlalbani@gmail.com>
2026-02-11 18:15:23 -0400

    refresh button in usercursos

--- resources/views/pages/admin/usuarios/usercursos.blade.php
+++ resources/views/pages/admin/usuarios/usercursos.blade.php
@@ -28,43 +28,41 @@
 <div class="row filtros">						
 	<div class="col-md-12">
 		<form class="form-horizontal">
-			<div class="form-group">
-		        <div class="col-md-4 col-sm-6 col-xs-12 margin-top">
-		            <label for="titulos" class="control-label">Título</label>
-		            <input type="text" class="form-control" id="titulos" name="titulos" value="{{$titulos}}" />
-		             
-		        </div>
-
-		        @if($usuario->isParticipante())
-			        <div class="col-md-4 col-sm-6 col-xs-12">
-			          	<label for="id_facilitador" class="control-label">Facilitador</label>
-
-			            <select name="id_facilitador" class="select2 " id="id_facilitador" data-allow-clear="true" required="true">
-		                                <option value='0'>Todos</option>
-
-							@foreach($facilitadores as $facilitador)
-
-	                            @if($id_facilitador == $facilitador->id)
-	                                <option value="{{$facilitador->person->facilitator->id}}" selected>{{$facilitador->person->name}} {{$facilitador->person->last_name}} C.I:{{$facilitador->person->id_format()}}</option>
-	                            @else 
-	                                <option value="{{$facilitador->person->facilitator->id}}">{{$facilitador->person->name}} {{$facilitador->person->last_name}} C.I:{{$facilitador->person->id_format()}}</option>
-	                            @endif
-	                        @endforeach		                
-			            </select>  
-			       	</div>
-		       	@endif
-
-		      	<div class="col-md-4 col-sm-6 col-xs-12">
-		          <label for="fechas" class="control-label">Por mes</label>
-		          <div class="input-group">
-		          	<input type='text' class='form-control dat' autocomplete="off" name='fechas' data-format="yyyy-mm" id='fechas' placeholder='Fechas' value='{{$fechas}}' />
-		          	<span class="input-group-btn"> <button class="btn btn-primary btn-lg" type="submit"><i class='entypo-search'></i></button> </span>
-		          </div>
-		        </div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<button class="btn btn-primary"><span>Mostrar todo</span></button>
+			<div class="form-group" style="display: flex; flex-wrap: wrap; align-items: flex-end; column-gap: 0; row-gap: 1rem; margin-bottom: 10px;">
+				<div class="col-md-4 col-sm-6 col-xs-12 margin-top">
+					<label for="titulos" class="control-label">Título</label>
+					<input type="text" class="form-control" id="titulos" name="titulos" value="{{$titulos}}" />
 				</div>
-	       	</div>
+
+				@if($usuario->isParticipante())
+					<div class="col-md-4 col-sm-6 col-xs-12">
+						<label for="id_facilitador" class="control-label">Facilitador</label>
+						<select name="id_facilitador" class="select2 form-control" id="id_facilitador" data-allow-clear="true" required="true">
+							<option value='0'>Todos</option>
+							@foreach($facilitadores as $facilitador)
+								@if($id_facilitador == $facilitador->id)
+									<option value="{{$facilitador->person->facilitator->id}}" selected>{{$facilitador->person->name}} {{$facilitador->person->last_name}} C.I:{{$facilitador->person->id_format()}}</option>
+								@else 
+									<option value="{{$facilitador->person->facilitator->id}}">{{$facilitador->person->name}} {{$facilitador->person->last_name}} C.I:{{$facilitador->person->id_format()}}</option>
+								@endif
+							@endforeach	                
+						</select>  
+					</div>
+				@endif
+
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<label for="fechas" class="control-label">Por mes</label>
+					<div class="input-group">
+						<input type='text' class='form-control dat' autocomplete="off" name='fechas' data-format="yyyy-mm" id='fechas' placeholder='Fechas' value='{{$fechas}}' />
+						<span class="input-group-btn"> <button class="btn btn-primary btn-lg" type="submit"><i class='entypo-search'></i></button> </span>
+					</div>
+				</div>
+				<div class="col-md-1 col-sm-12 col-xs-12" style="min-width:60px;">
+					<button type="button" class="btn btn-default btn-lg" onclick="window.location='{{ url('u/participantes/acciones/'. request()->segment(count(request()->segments())) . '') }}'">
+						<i class="fa fa-refresh"></i>
+					</button>
+				</div>
+			</div>
 			<!-- <div class="col-xs-12 margin-top text-center" style="margin-bottom: 10px;">
 	              <button type='submit' class='btn btn-primary'>Buscar <i class='fa fa-lg fa-search'></i></button>
 	        </div> -->
