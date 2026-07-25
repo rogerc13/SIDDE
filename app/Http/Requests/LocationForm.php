757bdfed895a641524ed35a82bfa1d41229ebca1
mnlalbani <mnlalbani@gmail.com>
2026-07-24 22:53:22 -0400

    feat: add LocationForm request validation

---
+++ app/Http/Requests/LocationForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Location;
+
+class LocationForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        switch($this->method())
+        {
+            
+            case 'POST':
+                return [                        
+                        'nombre'=>'required|string|max:'.Location::MAX_LENGTH_NAME.'|unique:locations,name',
+                       ]; 
+                
+            case 'PUT':
+                return [
+                        'nombre'=>'required|string|max:'.Location::MAX_LENGTH_NAME,
+                       
+                       ];
+            default:return[];
+        }
+    }
+
+        public function messages()
+    {
+        return [
+             
+            'required' => 'El campo :attribute es necesario.',
+            'string' => 'El campo :attribute debe ser una cadena caracteres.',
+            'max' => 'El campo :attribute debe contener maximo :max caracteres.',
+            'unique' => 'El campo :attribute ya existe.',
+
+            
+        ];
+    }
+}
