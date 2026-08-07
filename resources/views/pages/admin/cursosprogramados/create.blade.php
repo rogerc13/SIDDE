34539cf8f995828fe47b95c83117b70e104fef22
mnlalbani <mnlalbani@gmail.com>
2026-08-06 23:17:01 -0400

    Translate FullCalendar to Spanish in scheduling course modal

--- resources/views/pages/admin/cursosprogramados/create.blade.php
+++ resources/views/pages/admin/cursosprogramados/create.blade.php
@@ -310,6 +310,21 @@ function initWizardCalendar() {
         selectable: true,
         selectHelper: true,
         selectMinDistance: 5,
+        buttonText: {
+            today: 'Hoy',
+            month: 'Mes',
+            agendaWeek: 'Semana',
+            agendaDay: 'Día'
+        },
+        monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+        monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
+        dayNames: ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'],
+        dayNamesShort: ['Dom','Lun','Mar','Mié','Jue','Vie','Sáb'],
+        dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+        allDayText: 'Todo el día',
+        eventLimitText: function(n) {
+            return '+' + n + ' más';
+        },
         select: function(start, end) {
             openAddSessionModal(start, end);
             window.wizardCalendar.fullCalendar('unselect');
